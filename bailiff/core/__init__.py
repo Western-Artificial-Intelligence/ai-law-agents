4f9a576f7850e8b63efaac92233b6ad480198089
Ronit Longia <130603198+ronit567@users.noreply.github.com>
2025-11-11 04:16:01 -0500

    Revert "P0: introduce tokenizer utility"
    This reverts commit 4e20cb9e624aeed50a2c45b5bf90181936dfcbcf.

--- bailiff/core/__init__.py
+++ bailiff/core/__init__.py
@@ -10,7 +10,6 @@
 from .events import EventTag, ObjectionRuling, TrialLog, UtteranceLog
 from .logging import LogFactory, default_log_factory, mark_completed
 from .session import TrialSession
-from .tokenizer import Tokenizer
 
 __all__ = [
     "AgentBudget",
@@ -26,7 +25,6 @@ __all__ = [
     "TrialLog",
     "TrialSession",
     "UtteranceLog",
-    "Tokenizer",
     "default_log_factory",
     "mark_completed",
 ]
